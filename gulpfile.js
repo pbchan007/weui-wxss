6c49bea7f9f2e2b1c6eb90d23360aa0196513d54
pbchan007 <pbchan007@gmail.com>
2019-04-30 14:54:29 +0800

    gulp-px2rpx 2prx->1rpx

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var cssnano = require('gulp-cssnano');
 var header = require('gulp-header');
 var autoprefixer = require('autoprefixer');
 var pkg = require('./package.json');
+const px2rpx = require('gulp-px2rpx');
 
 gulp.task('watch', function() {
   gulp.watch('src/**', ['build:style', 'build:example']);
@@ -35,8 +36,15 @@ gulp.task('build:style', function() {
       rename(function(path) {
         path.extname = '.wxss';
       })
+    ).pipe(
+      px2rpx({
+        screenWidth: 375, // 设计稿屏幕, 默认750
+        wxappScreenWidth: 750, // 微信小程序屏幕, 默认750
+        remPrecision: 6 // 小数精度, 默认6
+      })
+      // 需把2rpx换位1rpx
     )
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist-rpx'));
 });
 gulp.task('build:example', function() {
   gulp
